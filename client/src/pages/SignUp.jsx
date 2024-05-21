361e6539e3abdc0bd0dcab17900808dcd5a413a7
Ayush Soni <ayushsoni0718@gmail.com>
2024-05-21 12:57:17 +0530

    complete signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Label, TextInput } from 'flowbite-react';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6'>
+        {/* left */}
+        <div className='flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                Ayush's
+              </span>
+              Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project. You can signup with your email and password or with Google. 
+          </p>
+        </div>
+        {/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Username' />
+              <TextInput type='text' placeholder='username' id='username' />
+            </div>
+            <div>
+              <Label value='Email' />
+              <TextInput type='email' placeholder='email' id='email' />
+            </div>
+            <div>
+              <Label value='Password' />
+              <TextInput type='password' placeholder='password' id='password' />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/signin' className='text-blue-500'>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
