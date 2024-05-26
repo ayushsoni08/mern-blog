670296ea9647658344d13250ab1b16ac3e671087
Ayush Soni <ayushsoni0718@gmail.com>
2024-05-26 16:00:09 +0530

    complete sidebar of the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 import FooterComponent from './components/FooterComponent';
+import PrivateRoute from './components/PrivateRoute';
  
 const App = () => {
   return (
@@ -18,7 +19,9 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path='/projects' element={<Projects />} />
       </Routes>
       <FooterComponent />
