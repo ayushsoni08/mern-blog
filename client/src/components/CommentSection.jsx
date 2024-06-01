08b61a98c6510a0c3079d342fbf6a2463ced537a
Ayush Soni <ayushsoni0718@gmail.com>
2024-06-01 12:16:07 +0530

    add edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -85,6 +85,14 @@ const CommentSection = ({ postId }) => {
         }
     };
 
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+            comments.map((c) => 
+                c._id === comment._id ? {...c, content: editedContent} : c
+            )
+        );
+    };
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ?
@@ -139,7 +147,7 @@ const CommentSection = ({ postId }) => {
                         key={comment._id}
                         comment={comment}
                         onLike={handleLike}
-
+                        onEdit={handleEdit}
                     />
                 ))}
             </>
