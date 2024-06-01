f1fb2bab6a7537fa86d6233ebce0ed9e69a152ee
Ayush Soni <ayushsoni0718@gmail.com>
2024-06-01 12:45:56 +0530

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,9 +4,9 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
 
-    const { currentUser} = useSelector(store => store.user);
+    const { currentUser } = useSelector(store => store.user);
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -120,13 +120,22 @@ const Comment = ({ comment, onLike, onEdit }) => {
                         </p>
                         {
                             currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                                <button
-                                    type='button'
-                                    className='text-gray-400 hover:text-blue-500'
-                                    onClick={handleEdit}
-                                >
-                                    Edit
-                                </button>
+                                <>
+                                    <button
+                                        type='button'
+                                        className='text-gray-400 hover:text-blue-500'
+                                        onClick={handleEdit}
+                                        >
+                                        Edit
+                                    </button>
+                                    <button
+                                        type='button'
+                                        onClick={() => onDelete(comment._id)}
+                                        className='text-gray-400 hover:text-red-500'
+                                    >
+                                        Delete
+                                    </button>
+                                </>
                             )
                         }
                     </div>
