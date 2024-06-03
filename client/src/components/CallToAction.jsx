312ad362a6e7b62e16e9441f61ff0672d29b22b2
Ayush Soni <ayushsoni0718@gmail.com>
2024-06-03 11:19:32 +0530

    update callToAction component

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -6,14 +6,14 @@ const CallToAction = () => {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to know about the JS projects built by Ayush?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Checkout these JavaScript Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://github.com/ayushsoni08" target='_blank' rel='noopener noreferrer'>
+                    JavaScript Projects
                 </a>
             </Button>
         </div>
